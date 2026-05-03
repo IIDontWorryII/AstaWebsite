a6b0e6d72d860b7af3fb403b7fefe9c1e97f15ac
IIDontWorryII <daniel.schukin@gmail.com>
2026-05-03 09:35:33 +0200

    AW-12 wire routes for all pages with Layout wrapper

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,26 @@
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import EventsSection from "./components/EventsSection";
-import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Gremien from "./pages/Gremien";
+import Eventkalender from "./pages/Eventkalender";
+import Baracke from "./pages/Baracke";
+import Sport from "./pages/Sport";
+import Kontakt from "./pages/Kontakt";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <main>
-        <Hero />
-        <EventsSection />
-      </main>
-      <Footer />
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/gremien" element={<Gremien />} />
+        <Route path="/eventkalender" element={<Eventkalender />} />
+        <Route path="/baracke" element={<Baracke />} />
+        <Route path="/sport" element={<Sport />} />
+        <Route path="/kontakt" element={<Kontakt />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
